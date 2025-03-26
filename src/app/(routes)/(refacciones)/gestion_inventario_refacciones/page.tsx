802f2722c632aaf1b45c9a843b3f56ddb1309f2b
HugoBG14 <al21510374@tapachula.tecnm.mx>
2025-03-26 11:45:21 -0600

    directorio api añadido, carpetas y archivos para componentes de gestion de inventarios en quimicos y refacciones

--- src/app/(routes)/(refacciones)/gestion_inventario_refacciones/page.tsx
+++ src/app/(routes)/(refacciones)/gestion_inventario_refacciones/page.tsx
@@ -10,7 +10,7 @@ export default function pag() {
       {/* Título con margen adaptativo */}
       <div className="mt-16 md:mt-24 lg:mt-32 mb-8 px-4">
         <h1 className="text-white text-xl md:text-3xl lg:text-4xl font-bold text-center">
-          VISTA DE DASHBOARD DE REFACCIONES
+          VISTA DE GESTION DE REFACCIONES
         </h1>
       </div>
     </div>
