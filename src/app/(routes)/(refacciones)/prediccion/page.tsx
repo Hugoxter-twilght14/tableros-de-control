6b147fda8900412ee8c6b0c972654fabc2bb1059
HugoBG14 <borrazhugo14@gmail.com>
2025-05-15 18:33:51 -0600

    proteccion de la ruta prediccion que evita entrar sin estar logueado

--- src/app/(routes)/(refacciones)/prediccion/page.tsx
+++ src/app/(routes)/(refacciones)/prediccion/page.tsx
@@ -1,7 +1,16 @@
 import React from 'react'
 import PrediccionArima from './PrediccionArima/PrediccionArima'
+import { auth } from "../../../../../auth";
+import { redirect } from "next/navigation";
 
-export default function page() {
+export default async function page() {
+  const session = await auth();
+    
+      // Si no hay sesión, redirige al login
+      if (!session || !session.user) {
+        redirect("/login");
+      }
+
   return (
     <div>
       <PrediccionArima/>
