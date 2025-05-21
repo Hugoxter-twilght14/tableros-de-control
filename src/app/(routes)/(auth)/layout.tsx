a7c8176822ba1ce7dab90983d1ea8069930d7fc5
HugoBG14 <borrazhugo14@gmail.com>
2025-05-21 13:24:38 -0600

    imagen de fondo corregida en el login

--- src/app/(routes)/(auth)/layout.tsx
+++ src/app/(routes)/(auth)/layout.tsx
@@ -1,18 +1,21 @@
+
 export default function AuthLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
   return (
-   <div className="h-screen w-full relative overflow-hidden">
+    <div className="h-screen w-full relative overflow-hidden">
       {/* Fondo oscuro y la imagen */}
       <div className="absolute inset-0 -z-10">
         <div className="h-full w-full bg-black">
-          <div className="h-full w-full bg-[url('/inelac_fondo_login.jpg')] bg-cover bg-no-repeat bg-center opacity-30" />
+          <div className="h-full w-full bg-[url('/iconos/inelac_fondo_login.jpg')] bg-cover bg-no-repeat bg-center opacity-30" />
         </div>
       </div>
-      <div className="flex lg:justify-start justify-center items-center h-full w-full pl-4 pr-8 py-5">
-      <div className="ml-6 bg-[#ffffff] px-6 md:px-8 py-6 md:py-8 mt-15 rounded-2xl shadow-lg max-w-xs sm:max-w-sm md:max-w-md lg:max-w-lg">
+
+      {/* Formulario centrado */}
+      <div className="flex items-center justify-center h-[calc(100vh-100px)]">
+        <div className="bg-black px-8 py-10 rounded-lg w-full max-w-md">
           {children}
         </div>
       </div>
